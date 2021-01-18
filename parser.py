85b1a3075c4caddea9d9eef74c2348179f360fbf
Nyxiative <carsten.j@protonmail.com>
2021-01-18 13:01:31 +0100

    Comments and small reduction of print calls

--- parser.py
+++ parser.py
@@ -1,20 +1,30 @@
-from os import replace, system
-import yaml
+# This is used for the windows VT1000 workaround
+from os import system 
+# This is used for parsing the game text files into data that the interpreter 
+# can understand
+import yaml 
+# This makes sure that the text that gets output is automatically wrapped to 
+# to multiple lines if the string is too long
 import textwrap
 
+# Some general global variables for storing game data
 game = {}
 location = {}
 triggers = {'end': False}
 
-
+# Load the game from a yaml file to a python data structure
 def load(filename):
 	with open(filename, 'r') as f:
+		# Load the file and crash if the file is incorrectly formatted
 		try:
 			return yaml.safe_load(f)
 		except yaml.YAMLError as exc:
 			print(exc)
 			exit(1)
 
+# This get the part of the game object that represents the location the player 
+# is currently located at
+# The format that is used in the game file is "location/room"
 def parse_location(string):
 	parts = string.split('/')
 	return game['locations'][parts[0]]['rooms'][parts[1]]
@@ -29,12 +39,13 @@ def main():
 	# after the proccess ends is a bug and shouldn't be relied on, but this is 
 	# the easiest way.
 	system('')
+	# Load the game into the global game variable and parse the starting 
+	# location
 	game = load('test.yml')
 	location = parse_location(game['start'])
 
-	print('\u001b[32;1m', end='')
-	print(f"The name is the loaded game is '{game['name']}'")
-	print('\u001b[0m', end='')
+	# Print the title of the game and the text of the starting location
+	print(f"\u001b[32;1mThe name is the loaded game is '{game['name']}'\u001b[0m")
 	print('\u001b[32m', end='')
 	print(textwrap.fill(location['message']))
 
@@ -47,6 +58,9 @@ def main():
 		print('\u001b[32m', end='')
 
 		# TODO: change this to allow multiple keywords for chagne location/room
+		# If the command contains the word enter for now, get which exit the 
+		# player is referring to in the command and load it into the location 
+		# variable and print the corresponding text
 		if 'enter' in cmd:
 			loc = get_exit_from_input(cmd)
 			if loc is not None:
@@ -65,6 +79,7 @@ def main():
 					else:
 						if 'message' in val:
 							print(textwrap.fill(val['message']))
+							# This is a huge mess but im not sure how to better do it
 						elif 'if' in val:
 							printed = False
 							for condition in val['if']:
@@ -132,6 +147,9 @@ def parse_condition(condition):
 	val = (res[1:], res[0]) if len(res) != 1 else (res, "&&")
 	return val
 
+# These 3 functions gets either an object, an action, or an exit based on the 
+# command and which location you are in
+
 def get_object_from_input(cmd):
 	for i in location['objects']:
 		names = get_name_and_alias(location['objects'][i], i)
@@ -154,7 +172,8 @@ def get_exit_from_input(cmd):
 				if isinstance(location['exits'][i.replace(' ', '_')], str):
 					return location['exits'][i.replace(' ', '_')]
 				return location['exits'][i.replace(' ', '_')]['exit']
-		
+	
+# This gets the name and aliases of the object as a list of strings
 def get_name_and_alias(obj, name):
 	if not isinstance(obj, str):
 		if 'alias' in obj:
